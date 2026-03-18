1e56b67eff84bd5d803f09f66003ee28c4a40bf4
Mikolaj Izdebski <mizdebsk@redhat.com>
2026-03-18 14:56:02 +0100

    Make Sanity/smoke a parametrized test

--- Sanity/smoke/runtest.sh
+++ Sanity/smoke/runtest.sh
@@ -3,33 +3,22 @@
 . /usr/share/beakerlib/beakerlib.sh
 
 : ${PACKAGE_NAME:=radii}
+: ${RADII:=radii}
 
 rlJournalStart
 
-  rlPhaseStartTest "check for presence of radii commands"
+  rlPhaseStartTest "check for presence of ${RADII} commands"
     rlAssertRpm ${PACKAGE_NAME}
-    rlAssertBinaryOrigin radii ${PACKAGE_NAME}
-    rlAssertBinaryOrigin rhel-drivers ${PACKAGE_NAME}
+    rlAssertBinaryOrigin ${RADII} ${PACKAGE_NAME}
   rlPhaseEnd
 
-  rlPhaseStartTest "display radii version"
-    rlRun -s "radii --version"
-    rlAssertGrep "^radii version " ${rlRun_LOG}
+  rlPhaseStartTest "display ${RADII} version"
+    rlRun -s "${RADII} --version"
+    rlAssertGrep "^${RADII} version " ${rlRun_LOG}
   rlPhaseEnd
 
-  rlPhaseStartTest "display rhel-drivers version"
-    rlRun -s "rhel-drivers --version"
-    rlAssertGrep "^rhel-drivers version " ${rlRun_LOG}
-  rlPhaseEnd
-
-  rlPhaseStartTest "display radii help"
-    rlRun -s "radii --help"
-    rlAssertGrep "^Usage:" ${rlRun_LOG}
-    rlAssertGrep "install" ${rlRun_LOG}
-  rlPhaseEnd
-
-  rlPhaseStartTest "display rhel-drivers help"
-    rlRun -s "rhel-drivers --help"
+  rlPhaseStartTest "display ${RADII} help"
+    rlRun -s "${RADII} --help"
     rlAssertGrep "^Usage:" ${rlRun_LOG}
     rlAssertGrep "install" ${rlRun_LOG}
   rlPhaseEnd
